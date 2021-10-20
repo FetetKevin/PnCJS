8066591e4799223ef30aa45c0e3b091b512c8118
FetetKevin <fetetkevin@gmail.fr>
2021-10-20 10:09:20 +0200

    site fil rouge avec du JS

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+const sr = ScrollReveal({
+    distance: '30px',
+    origin: 'top',
+    scale: 0.5,
+    duration: 800,
+    delay: 200
+}, 300);
+
+//SR INDEX
+sr.reveal('h1');
+sr.reveal('.navbar li');
+sr.reveal('img');
+sr.reveal('.item1');
+sr.reveal('.item2');
+sr.reveal('.item3');
+sr.reveal('.item4');
+sr.reveal('.headband');
+
+//SR STUDENTS
+sr.reveal('.single-img');
+
+//SR ELEVE
+sr.reveal('.container4');
+
+//SR BLOG
+sr.reveal('.article-pic');
+sr.reveal('.article-desc');
+sr.reveal('.pagination');
+sr.reveal('button');
+
+//SR CONTACT
+sr.reveal('.container5');
